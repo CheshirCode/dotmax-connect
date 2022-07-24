183342f87f448bde74291097a0877a66910159e0
CheshireCode <50378606+CheshirCode@users.noreply.github.com>
2022-07-25 00:21:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let arr1 = ['050', '100', '294', '358'];
+console.log(arr1);
+// let arr2 = arr1.splice(1, 1, '735', '777');
+// console.log(arr2);
+arr1.splice(0, 1, '735', '777');
+console.log(arr1);
